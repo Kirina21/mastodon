1bfc3bac179e1c8a4b71a4a8403f5cd2ad9a2b07
Genbu Hase <hasegenbu@gmail.com>
2018-04-05 19:58:21 +0900

    Remove codes of quote-notifications

--- app/javascript/mastodon/features/notifications/components/notification.js
+++ app/javascript/mastodon/features/notifications/components/notification.js
@@ -129,23 +129,6 @@ export default class Notification extends ImmutablePureComponent {
     );
   }
 
-  renderQuote (notification, link) {
-    return (
-      <HotKeys handlers={this.getHandlers()}>
-        <div className='notification notification-quote focusable' tabIndex='0'>
-          <div className='notification__message'>
-            <div className='notification__favourite-icon-wrapper'>
-              <i className='fa fa-fw fa-quote-right' />
-            </div>
-            <FormattedMessage id='notification.quote' defaultMessage='{name} quoted your status' values={{ name: link }} />
-          </div>
-
-          <StatusContainer id={notification.get('status')} account={notification.get('account')} muted withDismiss hidden={this.props.hidden} />
-        </div>
-      </HotKeys>
-    );
-  }
-
   render () {
     const { notification } = this.props;
     const account          = notification.get('account');
@@ -161,8 +144,6 @@ export default class Notification extends ImmutablePureComponent {
       return this.renderFavourite(notification, link);
     case 'reblog':
       return this.renderReblog(notification, link);
-    case 'quote':
-      return this.renderQuote(notification, link);
     }
 
     return null;
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -54,19 +54,6 @@ class NotificationMailer < ApplicationMailer
     end
   end
 
-  def quote(recipient, notification)
-    @me      = recipient
-    @account = notification.from_account
-    @status  = notification.target_status
-
-    return if @me.user.disabled? || @status.nil?
-
-    locale_for_account(@me) do
-      thread_by_conversation(@status.conversation)
-      mail to: @me.user.email, subject: I18n.t('notification_mailer.quote.subject', name: @account.acct)
-    end
-  end
-
   def follow_request(recipient, notification)
     @me      = recipient
     @account = notification.from_account
