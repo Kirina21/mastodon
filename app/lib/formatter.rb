16aa741e1bc77e6b6ce5efc91d4db55be9bba447
Genbu Hase <hasegenbu@gmail.com>
2018-04-08 16:03:51 +0900

    Test:

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -223,6 +223,6 @@ class Formatter
   end
 
   def quote_html(html)
-    encode(html.gsub(/(\[\w+\])(\[(?:https?|ftp):\/\/[\-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+\/users\/[\w\-_]+(?:\/statuses\/\w+)\])/, "<span class=\"invisible\">\\1</span>\\2")).html_safe
+    html.gsub(/(\[\w+\])(\[(?:https?|ftp):\/\/[\-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+\/users\/[\w\-_]+(?:\/statuses\/\w+)\])/, "<span class=\"invisible\">\\1</span>\\2").html_safe
   end
 end
